32235747a959a1f51d2ae5945c66c126ed0606ec
Outuby <outubyyt@gmail.com>
2022-06-30 19:54:43 +0200

    added copyright and new voices

--- assets/js/test.js
+++ assets/js/test.js
@@ -23,21 +23,29 @@ function playRandomSound(){
     var sounds = [
         "assets/audio/outuby_exeos.mp3",
         "assets/audio/outuby_skush.mp3",
+        "assets/audio/outuby_exeos.mp3",
+        "assets/audio/outuby_skush.mp3",
         "assets/audio/outuby_justin.mp3",
         "assets/audio/outuby_matthew.mp3",
         "assets/audio/outuby_joey.mp3",
         "assets/audio/outuby_petermann.mp3",
         "assets/audio/outuby_gaffi.mp3",
-        "assets/audio/outuby_exeos.mp3",
-        "assets/audio/outuby_skush.mp3",
-        "assets/audio/outuby_justin.mp3",
-        "assets/audio/outuby_matthew.mp3",
+        // "assets/audio/outuby_GLaDOS_15ai.wav",
+        // "assets/audio/outuby_HAL 9000_15ai.wav",
+        "assets/audio/outuby_Peter.wav",
+        // "assets/audio/outuby_Rise Kajukawa_15ai (1).wav",
+        // "assets/audio/outuby_Rise Kajukawa_15ai (2).wav",
+        "assets/audio/outuby_sans_15ai.wav",
+        // "assets/audio/outuby_Sentry Turret_15ai.wav",
+        // "assets/audio/outuby_SpongeBob_15ai.wav",
+        "assets/audio/outuby_Walter White.wav",
         
     ];
         
   
     var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
     document.getElementById("player").innerHTML="<embed src=\""+soundFile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+    console.log(soundFile)
 }
 
 
