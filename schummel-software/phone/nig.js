3cde754d6f22186e419670decdc4fcc54af2384c
Outuby <outubyyt@gmail.com>
2023-06-03 20:12:44 +0200

    archives added

---
+++ schummel-software/phone/nig.js
@@ -0,0 +1,5 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // window.location.replace("./phone/index.html");
+  }else{
+    window.location.replace("../");
+  }
